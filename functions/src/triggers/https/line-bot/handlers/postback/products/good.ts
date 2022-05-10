65de5fa2ea08a59b48764513db9248417adcdc14
hyodoblog <yusei.hyodo@gmail.com>
2022-05-11 05:34:33 +0900

    stripe purchase handlerのリファクタリング

--- functions/src/triggers/https/line-bot/handlers/postback/products/good.ts
+++ functions/src/triggers/https/line-bot/handlers/postback/products/good.ts
@@ -1,13 +1,27 @@
 import { PostbackEvent } from '@line/bot-sdk'
 import { lineClient } from '~/utils/line'
-import { getCustomer, getInvoiceUrl } from '~/utils/stripe'
+import { getCustomer, stripe } from '~/utils/stripe'
 import { errorLogger } from '~/utils/util'
 import { msgPurchase } from '~/notice-messages/purchase'
+
+const purchase = async (customerId: string, priceId: string): Promise<{ url: string }> => {
+  await stripe.invoiceItems.create({ customer: customerId, price: priceId })
+  const { id: invoiceId } = await stripe.invoices.create({
+    customer: customerId,
+    payment_settings: { payment_method_types: ['card', 'konbini'] },
+    collection_method: 'send_invoice',
+    days_until_due: 7
+  })
+  await stripe.invoices.sendInvoice(invoiceId)
+  const { hosted_invoice_url } = await stripe.invoices.retrieve(invoiceId)
+
+  return { url: hosted_invoice_url! }
+}
 
 export const postbackProductsGoodHandler = async (event: PostbackEvent, priceId: string): Promise<void> => {
   try {
     const customer = await getCustomer(event.source.userId!)
-    const url = await getInvoiceUrl(customer.id, priceId)
+    const { url } = await purchase(customer.id, priceId)
 
     await lineClient.replyMessage(event.replyToken, msgPurchase(url))
   } catch (err) {
--- functions/src/triggers/https/line-bot/handlers/postback/products/service.ts
+++ functions/src/triggers/https/line-bot/handlers/postback/products/service.ts
@@ -1,13 +1,35 @@
 import { PostbackEvent } from '@line/bot-sdk'
 import { lineClient } from '~/utils/line'
-import { getCheckoutSessionUrl, getCustomer } from '~/utils/stripe'
+import { getCustomer, stripe } from '~/utils/stripe'
 import { errorLogger } from '~/utils/util'
 import { msgPurchase } from '~/notice-messages/purchase'
+import { LINE_FRIEND_URL } from '~/utils/secrets'
+
+const purchase = async (customerId: string, priceId: string): Promise<{ url: string }> => {
+  const { url } = await stripe.checkout.sessions.create({
+    customer: customerId,
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1
+      }
+    ],
+    shipping_address_collection: {
+      allowed_countries: ['JP']
+    },
+    mode: 'subscription',
+    payment_method_types: ['card'],
+    success_url: LINE_FRIEND_URL,
+    cancel_url: LINE_FRIEND_URL
+  })
+
+  return { url: url! }
+}
 
 export const postbackProductsServiceHandler = async (event: PostbackEvent, priceId: string): Promise<void> => {
   try {
     const customer = await getCustomer(event.source.userId!)
-    const url = await getCheckoutSessionUrl(customer.id, priceId, 'service')
+    const { url } = await purchase(customer.id, priceId)
 
     await lineClient.replyMessage(event.replyToken, msgPurchase(url))
   } catch (err) {
