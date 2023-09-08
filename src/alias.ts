1285b71b9083a5e1bbf2c9e7e467fe881d92a9ba
hyodoblog <yusei.hyodo@gmail.com>
2023-09-08 19:08:59 +0900

    🚑 fix alias.

--- src/alias.ts
+++ src/alias.ts
@@ -3,5 +3,6 @@ import moduleAlias from 'module-alias'
 
 moduleAlias.addAliases({
   '~': join(__dirname, './'),
-  '~line': join(__dirname, './triggers/https/line-bot')
+  '~lineBot': join(__dirname, './routes/line-bot'),
+  '~stripeWebhook': join(__dirname, './routes/stripe-webhook')
 })
